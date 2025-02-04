be960c086c1e34e0300c85646ff2e01c5a42e523
Asker Khuzhokov <a7kerkh@yandex.ru>
2025-02-04 13:01:13 +0300

    removed sqlite

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	_ "embed"
 	"log/slog"
 	"net/http"
@@ -13,33 +12,21 @@ import (
 
 	"github.com/askerdev/unisync_bot/internal/cli"
 	"github.com/askerdev/unisync_bot/internal/mospolytech"
-	"github.com/askerdev/unisync_bot/internal/sqlite"
 	"github.com/askerdev/unisync_bot/internal/tg"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 const TOKEN_KEY = "TELEGRAM_BOT_API_TOKEN"
 const URL = "https://api.telegram.org/bot"
-const DB_FILE_NAME = "telegram_bot.db"
 const SCHEDULE_URL = "https://rasp.dmami.ru/semester.json"
 
 func main() {
 	httpclient := &http.Client{}
 
-	db, err := sql.Open("sqlite3", DB_FILE_NAME)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
 	bot := &tg.Bot{
 		Token:  os.Getenv(TOKEN_KEY),
 		Url:    URL,
 		Client: httpclient,
-	}
-
-	storage, err := sqlite.NewStorage(db)
-	if err != nil {
-		panic(err)
 	}
 
 	api := mospolytech.NewAPI(
@@ -47,7 +34,7 @@ func main() {
 		httpclient,
 	)
 
-	app := cli.New(os.Args, storage, api, bot)
+	app := cli.New(os.Args, api, bot)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 
@@ -68,7 +55,7 @@ func main() {
 		}
 	}()
 
-	if err := app.Run(ctx); err != nil {
+	if err := app.Run(); err != nil {
 		panic(err)
 	}
 }
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,13 +2,10 @@ package cli
 
 import (
 	"context"
-	"errors"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/askerdev/unisync_bot/internal/converter"
-	"github.com/askerdev/unisync_bot/internal/domain"
 	"github.com/askerdev/unisync_bot/internal/mospolytech"
 	"github.com/askerdev/unisync_bot/internal/tg"
 )
@@ -17,12 +14,6 @@ var GROUP = os.Getenv("GROUP")
 var CHAT_ID = os.Getenv("CHAT_ID")
 
 type Handler func(context.Context) error
-
-type storage interface {
-	Insert(context.Context, []*domain.Task) error
-	Select(context.Context) ([]*domain.Task, error)
-	Delete(context.Context, int) error
-}
 
 type telegramBot interface {
 	SendMessage(*tg.SendMessageParams) (*tg.Message, error)
@@ -34,113 +25,48 @@ type mospolytechAPI interface {
 
 type cli struct {
 	args     []string
-	storage  storage
 	bot      telegramBot
 	mpAPI    mospolytechAPI
 	handlers map[string]Handler
 }
 
-func New(args []string, storage storage, api mospolytechAPI, bot telegramBot) *cli {
+func New(args []string, api mospolytechAPI, bot telegramBot) *cli {
 	return &cli{
 		args:     args,
-		storage:  storage,
 		mpAPI:    api,
 		bot:      bot,
 		handlers: map[string]Handler{},
 	}
 }
 
-func (a *cli) test(ctx context.Context) error {
-	tasks, err := a.storage.Select(ctx)
-	if err != nil {
-		return err
-	}
-
-	m := &sync.Mutex{}
-	wg := &sync.WaitGroup{}
-	t := tasks[0]
-
-	wg.Add(1)
-	go func() {
-		m.Lock()
-		defer m.Unlock()
-		defer wg.Done()
-		a.storage.Delete(ctx, t.ID)
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		a.bot.SendMessage(&tg.SendMessageParams{
-			ChatID:    t.ChatID,
-			Text:      t.Text,
-			ParseMode: "HTML",
-		})
-	}()
-
-	wg.Wait()
-
-	return nil
-}
-
-func (a *cli) update(ctx context.Context) error {
+func (a *cli) notify() error {
 	sch, err := a.mpAPI.Schedule()
 	if err != nil {
 		return err
 	}
-	tasks, err := converter.TasksFromSchedule(CHAT_ID, "241-3210", sch)
+	tasks, err := converter.TasksFromSchedule(CHAT_ID, GROUP, sch)
 	if err != nil {
 		return err
 	}
 
-	return a.storage.Insert(ctx, tasks)
-}
-
-func (a *cli) notify(ctx context.Context) error {
-	tasks, err := a.storage.Select(ctx)
-	if err != nil {
-		return err
-	}
-
-	m := &sync.Mutex{}
-	wg := &sync.WaitGroup{}
 	now := time.Now().Unix()
 	for _, t := range tasks {
 		if t.TimeAt > now {
 			continue
 		}
-		wg.Add(1)
-		go func() {
-			m.Lock()
-			defer m.Unlock()
-			defer wg.Done()
-			a.storage.Delete(ctx, t.ID)
-		}()
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			_, err := a.bot.SendMessage(&tg.SendMessageParams{
-				ChatID:    t.ChatID,
-				Text:      t.Text,
-				ParseMode: "HTML",
-			})
-			if err != nil {
-				panic(err)
-			}
-		}()
+		_, err := a.bot.SendMessage(&tg.SendMessageParams{
+			ChatID:    t.ChatID,
+			Text:      t.Text,
+			ParseMode: "HTML",
+		})
+		if err != nil {
+			panic(err)
+		}
 	}
-
-	wg.Wait()
 
 	return nil
 }
 
-func (a *cli) Run(ctx context.Context) error {
-	a.handlers["update"] = a.update
-	a.handlers["notify"] = a.notify
-	a.handlers["test"] = a.test
-	action, ok := a.handlers[a.args[1]]
-	if !ok {
-		return errors.New("cli: unknown command")
-	}
-	return action(ctx)
+func (a *cli) Run() error {
+	return a.notify()
 }
